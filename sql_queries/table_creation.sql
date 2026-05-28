17992295906cc41dfa6915ccb1b1f786564186b9
Prakhar Sinha <sinha.prakhar96@gmail.com>
2026-05-28 12:26:58 +0530

    Update table_creation.sql

--- sql_queries/table_creation.sql
+++ sql_queries/table_creation.sql
@@ -1,1 +1,16 @@
-
+CREATE TABLE churn (
+    RowNumber INT,
+    CustomerId BIGINT,
+    Surname VARCHAR(100),
+    CreditScore INT,
+    Geography VARCHAR(50),
+    Gender VARCHAR(20),
+    Age INT,
+    Tenure INT,
+    Balance NUMERIC,
+    NumOfProducts INT,
+    HasCrCard INT,
+    IsActiveMember INT,
+    EstimatedSalary NUMERIC,
+    Exited INT
+);
